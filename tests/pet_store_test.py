9b2f3640c9e96c9f52456be4c95c37d636090006
TorresNahu <torres.nahu@gmail.com>
2021-11-19 10:24:52 -0300

    Refactored code in pet store test

--- tests/pet_store_test.py
+++ tests/pet_store_test.py
@@ -1,21 +1,10 @@
 import requests as requests
 from assertpy.assertpy import soft_assertions
 
-import data.pet_status_dict as pet_status_dict
-from pet_store.pet.base_pet import BasePet
-from tests.assertions.pets_assertions import *
+from pet_store.pet.pet_object import PetObject
+from tests.assertions.petstore_assertions import *
 
-pet = BasePet()
-
-
-def test_available_pets_has_expected_pet_name(pet_name="doggie"):
-    response = pet.get_pets_by_status(pet_status_dict.pet_status.get('AVAILABLE'))
-    with soft_assertions():
-        assert_response_status_code_is_valid(response, requests.codes.ok)
-        # assert_pet_name_exists(response, pet_name)
-        pets = response.json()
-        # pet_names = search_pets_by_condition(response, 'doggie', 'name')
-        # assert_that(pet_names).is_true()
+pet = PetObject()
 
 
 def test_add_new_pet_to_store(create_pet_data):
